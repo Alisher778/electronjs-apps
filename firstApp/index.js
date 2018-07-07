261e73caeabc507a5fd79179c25883b9f913cb75
Alisher778 <internet-docs@mail.ru>
2018-07-07 16:49:16 +0500

    app initial data has been successfully loaded

--- firstApp/index.js
+++ firstApp/index.js
@@ -1,16 +1,7 @@
-const electron = require('electron');
-const { app }  = electron;
+const electron = require("electron");
+const { app, BrowserWindow } = electron;
 
-// function createWindow () {
-//     // Create the browser window.
-//     win = new BrowserWindow({width: 800, height: 600})
-  
-//     // and load the index.html of the app.
-//     win.loadFile('./index.html')
-//   }
-  
-//   app.on('ready', createWindow);
-
-  app.on('ready', () => {
-    console.log('App is ready');
-  });
+app.on("ready", () => {
+  const mainWindow  = new BrowserWindow({});
+  mainWindow.loadURL(`file://${__dirname}/index.html`)
+});
