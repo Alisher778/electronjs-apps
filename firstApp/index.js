e60832fc464d065e1bfbebb691c3b111b00fc3c5
Alisher778 <internet-docs@mail.ru>
2018-07-10 17:40:20 +0500

    simple 1st app finished

--- firstApp/index.js
+++ firstApp/index.js
@@ -1,7 +1,16 @@
 const electron = require("electron");
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, ipcMain } = electron;
+const ffmpeg = require('fluent-ffmpeg');
 
+let mainWindow;
 app.on("ready", () => {
-  const mainWindow  = new BrowserWindow({});
+  mainWindow  = new BrowserWindow({});
   mainWindow.loadURL(`file://${__dirname}/index.html`)
+});
+
+ipcMain.on('videoSubmited', (event, path)  => {
+  ffmpeg.ffprobe(path, (err, metadata) => {
+    console.log(metadata.format.duration)
+    mainWindow.webContents.send('videoMetadata', metadata.format.duration)
+  });
 });
